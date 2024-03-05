dba0fa1b3fddf271e0f1d0a95992bb71b0576268
00017037 <wiut00017037@gmail.com>
2024-03-05 14:37:02 +0500

    feat: add mvc project

---
+++ DBSD_17037_16777_17286/DBSD_17037_16777_17286/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DBSD_17037_16777_17286.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
